11b58f945504d62b421188994eb65f772f323417
Juan Olivares Fuentealba <juanandres.olivares@externos-cl.cencosud.com>
2020-08-08 11:33:14 -0400

    WIP: Refactor

--- index.ts
+++ index.ts
@@ -1,46 +1,112 @@
+
 require('dotenv').config();
-const { google } = require('googleapis');
+const yts = require('yt-search')
 const Discord = require('discord.js');
 const ytdl = require('ytdl-core');
 const bot = new Discord.Client();
-const broadcast = bot.voice.createBroadcast();
+const queue = new Map();
+let index = 1;
+bot.commands = new Discord.Collection();
+const botCommands = require('./commands');
 
-// Each API may support multiple version. With this sample, we're getting
-// v3 of the blogger API, and using an API key to authenticate.
-
-const youtube = google.youtube({
-    version: 'v3',
-    auth: process.env.YOUTUBE_API_KEY,
+Object.keys(botCommands).map(key => {
+    bot.commands.set(botCommands[key].name, botCommands[key]);
 });
 
-const getVideo = async (videoReq: string) => {
 
-    const res = await youtube.search.list({
-        part: 'id,snippet',
-        q: videoReq,
-    });
-    return `https://www.youtube.com/watch?v=${res.data.items[0].id.videoId}`;
+
+bot.login(process.env.BOT_TOKEN);
+bot.on('ready', () => {
+    console.info(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on('message', async msg => {
+
+    const args = msg.content.split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (!bot.commands.has(command)) return;
+
+    try {
+        bot.commands.get(command).execute(msg, args);
+    } catch (error) {
+        console.error(error);
+        msg.reply('there was an error trying to execute that command!');
+    }
+    // if (message.content.startsWith('!tb')) {
+    //     const serverQueue = queue.get(message.guild.id);
+    //     execute(message, serverQueue);
+    // }
+    // if (message.content === '!skip' || message.content === '!next') {
+    //     const serverQueue = queue.get(message.guild.id);
+    //     serverQueue.connection.dispatcher.end();
+    // }
+    // if (message.content === '!queue' || message.content === '!q') {
+    //     // const serverQueue = queue.get(message.guild.id);
+    //     // message.channel.send(inspect(serverQueue.songProperties));
+    //     message.channel.send('Funcion en mantencion mi pana, se me cuida')
+    // }
+});
+
+
+const execute = async (message, serverQueue) => {
+    const query = message.content.slice(4);
+    const voiceChannel = message.member.voice.channel;
+    if (!serverQueue) {
+        const queueContruct = {
+            textChannel: message.channel,
+            voiceChannel: voiceChannel,
+            connection: null,
+            songs: [],
+            volume: 100,
+            playing: true,
+            songProperties: []
+        };
+        yts(query, async (error, response) => {
+            console.log(response.videos[0].author);
+            const video = response.videos[0].url;
+            queue.set(message.guild.id, queueContruct);
+            queueContruct.songs.push(video);
+            queueContruct.songProperties.push({ id: index, title: response.videos[0].title, image: response.videos[0].image, author: response.videos[0].author.name });
+            index++;
+            const connection = await voiceChannel.join();
+            queueContruct.connection = connection;
+            play(message.guild, queueContruct);
+        });
+    } else {
+        yts(query, async (error, response) => {
+            const video = response.videos[0].url;
+            serverQueue.songProperties.push({ id: index, title: response.videos[0].title, image: response.videos[0].image, author: response.videos[0].author.name });
+            index++;
+            serverQueue.songs.push(video);
+            return message.channel.send(`YAYA TITO ${video}`);
+        });
+    }
 }
 
-const botMessage = async () => {
+const play = (guild, customQueue) => {
 
-    bot.login(process.env.BOT_TOKEN);
-    bot.on('ready', () => {
-        console.info(`Logged in as ${bot.user.tag}!`);
-    });
+    const serverQueue = queue.get(guild.id);
+    const songTittle = customQueue.songProperties[0].title;
+    const song = customQueue.songs[0];
 
-    bot.on('message', async msg => {
-        if (msg.content.startsWith('!tb')) {
-            msg.reply('beep bop beep... buscando video...');
-            msg.member.voice.channel.join();
-            const video = await getVideo(msg.content.slice(4));
-            msg.channel.send(`Video URL: ${video}`);
-            broadcast.play(ytdl(video, { filter: 'audioonly', quality: 'highestaudio' }));
-            for (const connection of bot.voice.connections.values()) {
-                connection.play(broadcast);
-            }
-        }
-    });
+    serverQueue.connection
+        .play(ytdl(song, { filter: 'audioonly', quality: 'highestaudio' }))
+        .on("finish", () => {
+            serverQueue.songs.shift();
+            play(guild, serverQueue.songs[0]);
+        })
+        .on("error", error => console.error(error));
+
+    // dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+    // serverQueue.textChannel.send(`YAYAYA MUSICA TITO!!: ${song}`);
+
+    const exampleEmbed = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle(songTittle)
+        .setURL(song)
+        .setAuthor(customQueue.songProperties[0].author)
+        .setThumbnail(customQueue.songProperties[0].image)
+    serverQueue.textChannel.send(exampleEmbed);
+
 }
-
-botMessage();
